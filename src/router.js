75283dffd075d01b8e551c36b26d6c2eb2fafbd9
lysh121 <lysh121@qq.com>
2019-09-21 10:14:30 +0800

    评论列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -25,6 +25,10 @@ export default new Router({
         {
           path: '',
           component: Main
+        },
+        {
+          path: 'comment',
+          component: () => import('./views/comment')
         }
       ]
     }
---
+++ src/views/comment/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-card>
+        <bread-crumb slot="header">
+        <template slot='title'>评论列表</template>
+        </bread-crumb>
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
