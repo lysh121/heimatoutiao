6fd6f63a9be22ffc496b844981ee53ea1bda4e6c
lysh121 <lysh121@qq.com>
2019-09-23 14:59:37 +0800

    内容列表-文章列表样式

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -18,7 +18,6 @@
         </el-form-item>
         <el-form-item label="频道列表：">
           <el-select>
-
           </el-select>
         </el-form-item>
         <el-form-item label="时间选择：">
@@ -30,6 +29,24 @@
               </el-date-picker>
         </el-form-item>
       </el-form>
+    </el-card>
+    <el-card>
+      <div slot='header'>共找到55091条符合条件的内容</div>
+      <ul class="list">
+        <li class="list-item">
+          <img src="../../assets/img/404.png" alt="">
+          <dl>
+            <dt class="title">
+              123
+            </dt>
+            <dd>
+              <el-tag>已发表</el-tag>
+            </dd>
+            <dd class="date">2019-10-23 12:23:32</dd>
+          </dl>
+          <div class="right"><span><i class="el-icon-edit"></i>修改</span><span><i class="el-icon-delete"></i>删除</span></div>
+        </li>
+      </ul>
     </el-card>
   </div>
 </template>
@@ -43,4 +60,35 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.list-item{
+  display: flex;
+  img{
+    width: 180px;
+    margin-right: 20px;
+  }
+  dl{
+    flex: 1;
+    .title{
+      cursor: pointer;
+    }
+    dd{
+      margin: 10px 0;
+    }
+    .date{
+      font-size: 12px;
+      color: #999;
+    }
+  }
+
+  .right{
+    float: right;
+    font-size: 14px;
+
+    span{
+      margin-right: 5px;
+    }
+  }
+}
+
+</style>
